619baa54cb4510878f52186bfcdd401764769d06
Aleksei Verkholantcev <averkholantcev@Alekseis-MacBook-Pro.local>
2018-05-31 20:51:26 +1000

    Add terminfo files, change `install.sh`

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-FILES=$(ls -a | egrep -v "^\.$|^\.\.$|^\.git$|install\.sh")
+set -eu
 
-for FILE in $FILES; do
-    if [ -e ~/$FILE ]; then
+FILES_TO_LINK=$(find . -maxdepth 1 | sed 's/^\.\///g' | grep -Ev "^\\.$|^\\.\\.$|^\\.git$|^install\\.sh$|.+\\.terminfo")
+
+for FILE in $FILES_TO_LINK
+do
+    if [ -e "$HOME/$FILE" ]; then
         echo "File $FILE exists, it will be overriden. To cancel press Ctrl-C"
-        read
-        rm ~/$FILE
+        read -r
+        rm "$HOME/$FILE"
     fi
-    ln $FILE ~/$FILE
-    echo $FILE
+    echo ln "$FILE" "$HOME/$FILE"
+    ln "$FILE" "$HOME/$FILE"
+done
+
+TERMINFO_FILES=$(find . -maxdepth 1 | grep -E ".+\\.terminfo")
+
+for FILE in $TERMINFO_FILES
+do
+    echo tic -x "$FILE"
+    tic -x "$FILE"
 done
